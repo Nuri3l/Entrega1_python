0bfd4f582407431cfd3ea2b72086c48e876fb30d
Nahu <nahuel.cotta@gmail.com>
2025-03-24 16:40:08 -0300

    Agrego la parte del zip y corrijo una identacion que tenia mal

--- Entrega_1/Questions.py
+++ Entrega_1/Questions.py
@@ -28,13 +28,14 @@ correct_answers_index = [1, 2, 0, 3, 1]
 
 contador = 0
 
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
+for questions,answers, correct_answers_index in questions_to_ask:
 # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
 # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(questions)
+    for i, answer in enumerate(answers):
         print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -46,17 +47,19 @@ for _ in range(3):
             print("Respuesta no valida")
             exit(1)
 
-        if user_answer ==correct_answers_index[question_index]:
+        if user_answer ==correct_answers_index:
             print("Correcto")
             contador = contador + 1
             break        
-        else:
-# Si el usuario no responde correctamente después de 2 intentos,             
-# se muestra la respuesta correcta
-            print("Respuesta incorrecta.")
-            contador = contador - 0.5
-    print("La respuesta correcta es:")
-    print(answers[question_index][correct_answers_index[question_index]])
+    else:
+        # Si el usuario no responde correctamente después de 2 intentos,             
+        # se muestra la respuesta correcta
+        print("Respuesta incorrecta.")
+        contador = contador - 0.5
+        print("La respuesta correcta es:")
+        print(answers[correct_answers_index])
 # Se imprime un blanco al final de la pregunta
 print("El puntaje del jugador es:", contador )
+
+
 print()
